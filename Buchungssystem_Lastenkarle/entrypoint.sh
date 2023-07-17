92f57d2b643713f0796152fd65d1bc802ba6a364
uqfol <uqfol@student.kit.edu>
2023-07-17 14:19:46 +0200

    shellscript now makes migrations

--- Buchungssystem_Lastenkarle/entrypoint.sh
+++ Buchungssystem_Lastenkarle/entrypoint.sh
@@ -11,6 +11,10 @@ until mysqladmin ping -h"$host" -P"$port" --silent; do
 done
 echo "Execute migration Command"
 python manage.py collectstatic --no-input
+python manage.py makemigrations db_model
+python manage.py makemigrations api
+python manage.py makemigrations PrivacyStatement
+
 python manage.py migrate --no-input
 echo "Start Server command"
 gunicorn Buchungssystem_Lastenkarle.wsgi:application --bind 0.0.0.0:8000
