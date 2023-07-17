494bc21f2968166dfcb76f9615bb0adeffdb5a16
Dan <uwwav@student.ki.edu>
2023-07-17 13:51:51 +0200

    Startup now works with crash needs a fix but Merge so Jan can work with the new Project Structure now

--- Buchungssystem_Lastenkarle/Buchungssystem_Lastenkarle/asgi.py
+++ Buchungssystem_Lastenkarle/Buchungssystem_Lastenkarle/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Buchungssytem_Lastenkarle project.
+ASGI config for Buchungssystem_Lastenkarle project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Buchungssytem_Lastenkarle.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Buchungssystem_Lastenkarle.settings')
 
 application = get_asgi_application()
--- Buchungssystem_Lastenkarle/Buchungssystem_Lastenkarle/urls.py
+++ Buchungssystem_Lastenkarle/Buchungssystem_Lastenkarle/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Buchungssytem_Lastenkarle project.
+URL configuration for Buchungssystem_Lastenkarle project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
