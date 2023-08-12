27f5243665944b59a3ce7c02dc69bd7704369dca
uqfol <uqfol@student.kit.edu>
2023-08-12 12:05:29 +0200

    All APIs implemented (missing few functionalities, mainly OIDC user and minor things). Testing of Admin API necessary

--- Buchungssystem_Lastenkarle/api/urls.py
+++ Buchungssystem_Lastenkarle/api/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('booking/', include('api.booking.urls')),
     path('user/', include('api.user.urls')),
     path('manager/', include('api.manager.urls')),
+    path('admin/', include('api.admin.urls'))
 ]
