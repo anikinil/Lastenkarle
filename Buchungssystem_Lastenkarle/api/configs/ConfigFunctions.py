30c135a19255069a18563b5d3e03c440baefb0a1
DanielAlJackson <Daniel.Lauer.2001@web.de>
2023-08-21 23:19:34 +0200

    added all intended new features

--- Buchungssystem_Lastenkarle/api/configs/ConfigFunctions.py
+++ Buchungssystem_Lastenkarle/api/configs/ConfigFunctions.py
@@ -1,9 +1,6 @@
 import json
-
-
-class Store:
-    def __init__(self, name):
-        self.name = name
+import random
+import string
 
 
 def _generateStoreConfig(store_configs):
@@ -11,29 +8,44 @@ def _generateStoreConfig(store_configs):
         json.dump(store_configs, config_file, indent=2)
 
 
-# def update_store_open_hours(store_name, new_open_hours):
-#    store_configs = readStoreConfig()
-#
-#    for store_config in store_configs:
-#        if store_config["name"] == store_name:
-#            store_config["openhours"].update(new_open_hours)
-#
-#    _generateStoreConfig(store_configs)
+def update_store_config(store_name, new_store_infos):
+    stores = _readStoreConfig()
 
-
-def update_store_opening_hours(store_name, new_store_infos):
-    stores = readStoreConfig()
-    new_store_infos = json.load(new_store_infos)
-    print(new_store_infos)
     for store in stores:
         if store["name"] == store_name:
-            for new_store_info in new_store_infos:
-               store["openhours"].update(new_store_infos)
-    store_configs = {"stores": stores}  # Wrap the list of stores in a dictionary with "stores" key
+            if "openhours" in new_store_infos:
+                store["openhours"].update(new_store_infos["openhours"])
+            if "pretime" in new_store_infos:
+                store["openhours"]["pretime"] = new_store_infos["pretime"]
+            _generateStoreConfig({"stores": stores})
+            break
+
+
+def getStoreConfig(Storename):
+    stores = _readStoreConfig()
+    for store in stores:
+        if store["name"] == Storename:
+            return json.dumps(store)
+
+
+def getAllStoresConfig():
+    stores = _readStoreConfig()
+    return json.dumps(stores)
+
+
+def deleteStoreConfig(store_name):
+    stores = _readStoreConfig()
+    updated_stores = [store for store in stores if store["name"] != store_name]
+    store_configs = {"stores": updated_stores}
     _generateStoreConfig(store_configs)
 
 
-def readStoreConfig():
+def getStoreNameFromJson(StoreJson):
+    store = json.loads(StoreJson)
+    return store['name']
+
+
+def _readStoreConfig():
     try:
         with open("store_configs.json", "r") as config_file:
             store_configs = json.load(config_file)
@@ -43,7 +55,7 @@ def readStoreConfig():
 
 
 def add_store(store_name):
-    stores = readStoreConfig()
+    stores = _readStoreConfig()
     new_store = {
         "name": store_name,
         "openhours": {
@@ -81,9 +93,10 @@ def add_store(store_name):
                 "opened": True,
                 "start": "%H:%M",
                 "end": "%H:%M"
-            }
-        },
-        "pretime": "%H:%M"
+            },
+            "pretime": "%H:%M"
+        }
+
     }
     stores.append(new_store)
 
@@ -91,24 +104,37 @@ def add_store(store_name):
 
     _generateStoreConfig(store_configs)
 
+#here btw the code to genreate random string from alma todo pls move this someewhere else
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits  # Combining letters and digits
+    random_string = ''.join(random.choices(characters,k=length))
+    return random_string
 
-update_data_json = '''
-    {
-        "wed": {
-            "opened": true,
-            "start": "10:30",
-            "end": "20:00"
-        },
-        "thu": {
-            "opened": false,
-           "start": "%H:%M",
-            "end": "%H:%M"
-        }
-    }
-    '''
-# add_store("Store69")
 
-add_store("Store2")
-#update_data = json.loads(update_data_json)
-#store_to_update = "Store2"
-#update_store_opening_hours(store_to_update, update_data_json)
+#todo  delete example interaction before merging
+#add_store("Store1")
+#add_store("Store2")
+#add_store("Store27")
+#add_store("Store69")
+##Example usage
+#new_store_data_json = '''
+# {
+#    "openhours": {
+#        "wed": { "opened": true, "start": "20:00", "end": "21:00" },
+#        "fri": { "opened": true, "start": "13:00", "end": "16:00" },
+#        "sun": { "opened": false, "start": "%H:%M", "end": "%H:%M" }
+#    },
+#    "pretime": "20:00"
+# }
+# '''
+#store_name = "Store2"
+
+#new_store_data = json.loads(new_store_data_json)
+#
+#update_store_config(store_name, new_store_data)
+#print(getStoreConfig("Store2"))
+#print(getStoreNameFromJson(
+#    '{"name": " HiddenStore", "openhours": {"mon": {"opened": true, "start": "%H:%M", "end": "%H:%M"}, "tue": {"opened": true, "start": "%H:%M", "end": "%H:%M"}, "wed": {"opened": true, "start": "20:00", "end": "21:00"}, "thu": {"opened": true, "start": "%H:%M", "end": "%H:%M"}, "fri": {"opened": true, "start": "13:00", "end": "16:00"}, "sat": {"opened": true, "start": "%H:%M", "end": "%H:%M"}, "sun": {"opened": false, "start": "%H:%M", "end": "%H:%M"}, "pretime": "20:00"}}'))
+#print(getAllStoresConfig())
+#deleteStoreConfig('Store27')
+#print(getAllStoresConfig())
