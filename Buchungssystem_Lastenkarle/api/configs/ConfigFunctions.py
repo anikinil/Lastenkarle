8cbcda28af88d8e15a1f53f840396670025db670
DanielAlJackson <Daniel.Lauer.2001@web.de>
2023-08-21 22:24:41 +0200

    adding stores works but not updating the configs

--- Buchungssystem_Lastenkarle/api/configs/ConfigFunctions.py
+++ Buchungssystem_Lastenkarle/api/configs/ConfigFunctions.py
@@ -1,47 +1,114 @@
 import json
 
+
 class Store:
     def __init__(self, name):
         self.name = name
+
+
+def _generateStoreConfig(store_configs):
+    with open("store_configs.json", "w") as config_file:
+        json.dump(store_configs, config_file, indent=2)
+
+
+# def update_store_open_hours(store_name, new_open_hours):
+#    store_configs = readStoreConfig()
+#
+#    for store_config in store_configs:
+#        if store_config["name"] == store_name:
+#            store_config["openhours"].update(new_open_hours)
+#
+#    _generateStoreConfig(store_configs)
+
+
+def update_store_opening_hours(store_name, new_store_infos):
+    stores = readStoreConfig()
+    new_store_infos = json.load(new_store_infos)
+    print(new_store_infos)
+    for store in stores:
+        if store["name"] == store_name:
+            for new_store_info in new_store_infos:
+               store["openhours"].update(new_store_infos)
+    store_configs = {"stores": stores}  # Wrap the list of stores in a dictionary with "stores" key
+    _generateStoreConfig(store_configs)
+
 
 def readStoreConfig():
     try:
         with open("store_configs.json", "r") as config_file:
             store_configs = json.load(config_file)
-            stores = [Store(store_config["name"]) for store_config in store_configs]
-            return stores
+            return store_configs.get("stores", [])  # Get the "stores" key or return an empty list
     except FileNotFoundError:
         return []
 
-def generateStoreConfig(stores):
-    store_configs = []
-
-    for store in stores:
-        config = {
-            "name": store.name,
-            "openhours": {
-                "mon": {
-                    "opened": True,
-                    "start": "%H:%M",
-                    "end": "%H:%M"
-                },
-                # ... (same as before)
-            },
-            "pretime": "%H:%M"
-        }
-        store_configs.append(config)
-
-    with open("store_configs.json", "w") as config_file:
-        json.dump(store_configs, config_file, indent=2)
 
 def add_store(store_name):
     stores = readStoreConfig()
-    new_store = Store(store_name)
+    new_store = {
+        "name": store_name,
+        "openhours": {
+            "mon": {
+                "opened": True,
+                "start": "%H:%M",
+                "end": "%H:%M"
+            },
+            "tue": {
+                "opened": True,
+                "start": "%H:%M",
+                "end": "%H:%M"
+            },
+            "wed": {
+                "opened": True,
+                "start": "%H:%M",
+                "end": "%H:%M"
+            },
+            "thu": {
+                "opened": True,
+                "start": "%H:%M",
+                "end": "%H:%M"
+            },
+            "fri": {
+                "opened": True,
+                "start": "%H:%M",
+                "end": "%H:%M"
+            },
+            "sat": {
+                "opened": True,
+                "start": "%H:%M",
+                "end": "%H:%M"
+            },
+            "sun": {
+                "opened": True,
+                "start": "%H:%M",
+                "end": "%H:%M"
+            }
+        },
+        "pretime": "%H:%M"
+    }
     stores.append(new_store)
 
-    generateStoreConfig(stores)
+    store_configs = {"stores": stores}  # Wrap the list of stores in a dictionary with "stores" key
 
-# Example usage
-add_store("Store1")
+    _generateStoreConfig(store_configs)
+
+
+update_data_json = '''
+    {
+        "wed": {
+            "opened": true,
+            "start": "10:30",
+            "end": "20:00"
+        },
+        "thu": {
+            "opened": false,
+           "start": "%H:%M",
+            "end": "%H:%M"
+        }
+    }
+    '''
+# add_store("Store69")
+
 add_store("Store2")
-add_store("Store3")
+#update_data = json.loads(update_data_json)
+#store_to_update = "Store2"
+#update_store_opening_hours(store_to_update, update_data_json)
