6b2642d247a6f87e1efe50b3c9559c42ce04abc4
Carolin Schmuck <ukhqe@student.kit.edu>
2023-08-31 15:32:07 +0200

    Test tests added

--- Buchungssystem_Lastenkarle/api/admin/v1/tests.py
+++ Buchungssystem_Lastenkarle/api/admin/v1/tests.py
@@ -1,7 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIRequestFactory
+from .views import *
+
 
 class HelloWorldTestCase(TestCase):
     def test_hello_world(self):
         self.assertEqual("Hello, World!", "Hello, World!")
 
-# You can add more test cases here
+
+class AllUserFlagsTest(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+        self.user.is_superuser = True
+        self.user.save()
+
+    def test_get_all_user_flags(self):
+        request = self.factory.get('/user_flags/')
+        request.user = self.user
+        response = AllUserFlags.as_view()(request)
+
+        self.assertEqual(response.status_code, 200)
