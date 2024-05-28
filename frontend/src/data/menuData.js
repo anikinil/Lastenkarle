870d23cd7d793c632b4ef319655ffb25d0662a4d
anikinil <anikins.ilja@gmail.com>
2024-05-28 13:41:07 +0200

    changed booking nav bar

--- frontend/src/data/menuData.js
+++ frontend/src/data/menuData.js
@@ -5,7 +5,8 @@ import { FaUser } from "react-icons/fa";
 
 export const menuItems = [
     {
-        title: 'Rent',
+        title: 'Booking',
+        url: '/booking',
         roles: ['customer', 'manager', 'admin'],
         submenu: [
             {
