53ec44216537500146b46fbb5bb9e0c5a882e0c0
Alma <unycp@student.kit.edu>
2024-05-28 14:17:02 +0200

    Booking pages

---
+++ frontend/src/features/booking/pages/BikeBooking.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//Standard page for a Bike
+//TODO: Add bike description
+//TODO: Add Store information
+//TODO: Add calandar as availability overview
+const BikeBooking = () => {
+    return (
+        <h1>BikeName</h1>
+    );
+};
+  
+export default BikeBooking;
---
+++ frontend/src/features/booking/pages/RegionalBooking.js
@@ -0,0 +1,12 @@
+import React from "react";
+//Standard page for a specific region
+//TODO: Add Map of Region with store markers
+//TODO: Add calandar table for bikes in region
+//TODO: Add bike list of region
+const RegionalBooking = () => {
+    return (
+        <h1>RegionName</h1>
+    );
+};
+  
+export default RegionalBooking;
