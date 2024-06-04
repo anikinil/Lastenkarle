3bd2f7659efcf318c011356c63a917a30e00e48b
anikinil <anikins.ilja@gmail.com>
2024-06-04 15:07:02 +0200

    added TODOs

--- frontend/src/features/bikeList/components/BikeListItem.js
+++ frontend/src/features/bikeList/components/BikeListItem.js
@@ -8,6 +8,11 @@ const BikeListItem = ({bike, index}) => {
     const { t } = useTranslation();
 
     return (
+
+        // FIXME: perhaps add separate panels for each list item
+
+        // LEARN: think about how to display different buttons to different roles
+
         <div className="list-item" key={index}>
             <p className="label">{bike.name}</p>
             <button type="button" className="button">{t('book')}</button>
