058558e50c4d0493795d8ee8043c4fc996f93955
anikinil <anikins.ilja@gmail.com>
2024-06-06 21:28:12 +0200

    made bike list item panels and buttons interactive

--- frontend/src/features/bikeList/components/BikeListItem.js
+++ frontend/src/features/bikeList/components/BikeListItem.js
@@ -9,13 +9,38 @@ const BikeListItem = ({bike}) => {
 
     const { t } = useTranslation();
     
-    // THINK: how to display different buttons to different roles
+    // THINK how to display different buttons to different roles
+    // THINK maybe show big preview of bike image on lcik on miniature preview
+
+    const handlePanelClick = () => {
+        // TODO implement
+        console.log(bike.name)
+    }
+
+    const handleBookingsClick = e => {
+        // TODO implement
+        console.log("bookings")
+        e.stopPropagation()
+    }
+
+    const handleStoreClick = e => {
+        // TODO implement
+        console.log("store")
+        e.stopPropagation()
+    }
+
+    const handleDeleteClick = e => {
+        // TODO implement
+        console.log("delete")
+        e.stopPropagation()
+    }
+
     return (
-        <li className="list-item">
+        <li className="list-item" onClick={handlePanelClick}>
             <p className="label">{bike.name}</p>
-            <button type="button" className="button regular">{t('bookings')}</button>
-            <button type="button" className="button regular">{t('store')}</button>
-            <button type="button" className="button delete">{<MdDelete />}</button>
+            <button type="button" className="button regular" onClick={handleBookingsClick}>{t('bookings')}</button>
+            <button type="button" className="button regular" onClick={handleStoreClick}>{t('store')}</button>
+            <button type="button" className="button delete" onClick={handleDeleteClick}>{<MdDelete />}</button>
             <div className="img-container">
                 <img className="img" alt={bike.name} src={bike.image}></img>
             </div>
