da97c0926d2d773f0a5eb96898fe3eaf34878fc9
anikinil <anikins.ilja@gmail.com>
2024-10-30 17:23:59 +0100

    added fetching to StorePage.js

--- frontend/src/features/storeDisplay/pages/StorePage.js
+++ frontend/src/features/storeDisplay/pages/StorePage.js
@@ -1,44 +1,37 @@
 //Page of a singular store
 //TODO: Add Liste of bikes belonging to store
 //Consists of Name, Picture, Description and Information
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import PictureAndDescriptionField from '../../../components/display/pictureAndDescriptionField/PictureAndDescriptionField';
 import { useParams } from 'react-router-dom';
 import SingleLineTextField from '../../../components/display/SingleLineTextField';
-
-// TODO implement fetching
-let stores = [
-    {
-        id: 1,
-        name: 'Store 1',
-        image: require('../../../assets/images/store1.jpg'),
-        description: 'This is a description of Store 1',
-        address: 'Musterstraße 123, 76137 Karlsruhe'
-    },
-    {
-        id: 2,
-        name: 'Store 2',
-        image: require('../../../assets/images/store1.jpg').default,
-        description: 'This is a description of Store 2',
-        address: 'Musterstraße 123, 76137 Karlsruhe'
-    },
-    {
-        id: 3,
-        name: 'Store 3',
-        image: null,
-        description: 'This is a description of Store 3',
-        address: 'Musterstraße 123, 76137 Karlsruhe'
-    }
-]
+import { STORE_NAME, STORE_PAGE_BY_STORE_NAME } from '../../../constants/URIs/ManagerURI';
+import { ERR_FETCHING_STORE } from '../../../constants/ErrorMessages';
 
 const StorePage = () => {
 
     const { t } = useTranslation();
 
-    const { id } = useParams();
-    const store = stores.find(s => s.id === parseInt(id));
+    const { storeName } = useParams();
+
+    const [store, setStore] = useState();
+
+    const fetchStore = () => {
+        fetch(STORE_PAGE_BY_STORE_NAME.replace(STORE_NAME, storeName))
+            .then(response => response.json())
+            .then(data => {
+                setStore(data);
+            })
+            .catch(error => {
+                console.error(ERR_FETCHING_STORE, error);
+            });
+    }
+
+    useEffect(() => {
+        fetchStore();
+    });
 
     return (
         <>
