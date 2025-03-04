e383caafb74502b790d09d20063bdabea794661e
anikinil <anikins.ilja@gmail.com>
2025-03-04 18:08:37 +0100

    add GeneralFilterPage and RegionalFilterPage components; update Home.js to include GeneralFilterPage

---
+++ frontend/src/features/availabilityFilter/pages/GeneralFilterPage.js
@@ -0,0 +1,14 @@
+// Shows availabilities of all bikes of all regions filtered by selected dates
+
+import React from 'react';
+
+const GeneralFilterPage = () => {
+    
+    return (
+        <div>
+            <h1>General Filter Page</h1>
+        </div>
+    );
+}
+
+export default GeneralFilterPage;
